f8970695c6e799801a66ffae07a4a839c7eb7b25
ncorazza12 <rm562265@fiap.com.br>
2025-08-20 20:48:49 -0300

    Configuração backend S3 e pipeline Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,13 @@
 # PROVIDER
 terraform {
-
   required_version = "~> 1.12.2"
+
+  backend "s3" {
+    bucket         = "aws-virtual-machine-nickolas-corazza"  # seu bucket já criado
+    key            = "tfstate/terraform.tfstate"             # caminho dentro do bucket
+    region         = "us-east-1"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
@@ -9,12 +15,8 @@ terraform {
       version = "~> 6.9.0"
     }
   }
-
 }
 
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
+  region = "us-east-1"
 }
